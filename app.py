366d85d4a159d75cd87c4b186830d7ffa9d8926b
Shubham Shinde <shindeshubham0520@gmail.com>
2024-04-26 11:06:57 +0530

    Completed

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv() ##load all the nevironment variables
+import os
+import google.generativeai as genai
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+prompt="""You are Yotube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+within 250 words. Please provide the summary of the text given here:  """
+
+
+def extract_transcript_details(youtube_video_url):
+    try:
+        video_id=youtube_video_url.split("=")[1]
+        print(video_id)
+        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+
+        return transcript
+
+    except Exception as e:
+        raise e
+    
+## getting the summary based on Prompt from Google Gemini Pro
+def generate_gemini_content(transcript_text,prompt):
+
+    model=genai.GenerativeModel("gemini-pro")
+    response=model.generate_content(prompt+transcript_text)
+    return response.text
+
+st.title("Video Summary")
+youtube_link = st.text_input("Enter Video Link:")
+
+if youtube_link:
+    video_id = youtube_link.split("=")[1]
+
+    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+
+if st.button("Get Summary"):
+    transcript_text=extract_transcript_details(youtube_link)
+
+    if transcript_text:
+        summary=generate_gemini_content(transcript_text,prompt)
+        st.markdown("## Summary:")
+        st.write(summary)
+
